cb0b8b7028eb49c3b92633b13437b637e4ac35b1
Timothy03062 <36873809+Timothy03062@users.noreply.github.com>
2018-03-12 18:14:12 +1100

    Exe1

--- week2/exercise1.py
+++ week2/exercise1.py
@@ -13,23 +13,23 @@ print("hello! Let's get started")  # it printed "hello! Let's get started"
 
 some_words = ['what', 'does', 'this', 'line', 'do', '?']
 
-for word in some_words:
-    print(word)
+for word in some_words:# It will not print word 
+    print(word)#not printed
 
-for x in some_words:
-    print(x)
+for x in some_words:#it will not print
+    print(x)#not printed
 
-print(some_words)
+print(some_words)#It will not print
 
-if len(some_words) > 3:
-    print('some_words contains more than 3 words')
+if len(some_words) > 3:#It checks the length of the argument(Some_words). Any characters stored within the argument that is bigger than 3 characters will be printed
+    print('some_words contains more than 3 words') #'some_words contains more than 3 words') will be printed
 
-def usefulFunction():
+def usefulFunction():#The command will be name the six attributes of the computer ,(retursn a namedtuple)
     """
     You may want to look up what uname does before you guess
     what the line below does:
     https://docs.python.org/3/library/platform.html#platform.uname
     """
-    print(platform.uname())
+    print(platform.uname())#returned system, node, release, version, machine and processor 
 
 usefulFunction()
