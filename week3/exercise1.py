2bee0ee743b0df86205a2776d09f2e45c412d777
Timothy03062 <36873809+Timothy03062@users.noreply.github.com>
2018-03-20 16:23:28 +1100

    Excerise 1

--- week3/exercise1.py
+++ week3/exercise1.py
@@ -12,15 +12,35 @@ def loop_ranger(start, stop=None, step=1):
 
     Do this using any method apart from just using range()
     """
-    pass
+    returnList = [] #Only works with For loop
 
+    for i in range(start, stop, step):
+        returnList.append(i)
+    return returnList 
+
+    returnList = []
+
+    start += step
+     
+    while start < stop:
+        returnList.append(start)
+        start += step
+
+    return(start, returnList)
+
+
+    
 
 def lone_ranger(start, stop, step):
     """Duplicate the functionality of range.
 
     Look up the docs for range() and wrap it in a 1:1 way
     """
-    pass
+    rangelist = []
+    while start < stop:
+        rangelist.append(start)
+        start += step
+    return rangelist
 
 
 def two_step_ranger(start, stop):
@@ -29,16 +49,42 @@ def two_step_ranger(start, stop):
     Sometimes you want to hide complexity.
     Make a range function that always has a step size of 2
     """
-    pass
+    rangeList = []
+    while start < stop:
+        rangeList.append(start)
+        start += 2
+    return(rangeList)
+
 
 
 def gene_krupa_range(start, stop, even_step, odd_step):
     """Make a range that has two step sizes.
+    start=1,stop=20,even_step=3,odd_step=6
+
+    [1,7,10,16,19] (1st elemnt, 2nd element...) 
+
+    [1,4,10,13,19] (0th element,1stelem....)
 
     make a list that instead of having evenly spaced steps
     has odd steps be one size and even steps be another.
     """
-    pass
+    rangeList = []
+    
+
+    i = 0
+
+    while start < stop:
+        if i == 0:
+            rangeList.append(start)
+            start += odd_step
+            i = 1
+        else:
+            rangeList.append(start)
+            start += even_step
+            i = 0
+        
+    return(rangeList)
+
 
 
 def stubborn_asker(low, high):
@@ -47,7 +93,15 @@ def stubborn_asker(low, high):
     Ask for a number, and if the response is outside the bounds keep asking
     until you get a number that you think is OK
     """
-    pass
+    number = int(input("Enter a number between " + str(low) + " and " + str(high) + ": "))
+
+    while number <= int(low) or number >= int(high):
+        
+        number = int(input("Invalid. Enter a number between " + str(low) + " and " + str(high) + ": "))
+    print('OK')
+    return(number) 
+    
+    
 
 
 def not_number_rejector(message):
@@ -57,7 +111,14 @@ def not_number_rejector(message):
     "six", "8!") then throw it out and ask for an actual number.
     When you do get a number, return it.
     """
-    pass
+    while True:
+        try:
+            input_number = int(input(message))
+            print("Thanks! {} looks good.".format(input_number))
+            return input_number
+        except Exception as x:
+            print("Try again ({})".format(x))
+
 
 
 def super_asker(low, high):
@@ -66,7 +127,30 @@ def super_asker(low, high):
     Combine stubborn_asker and not_number_rejector to make a function
     that does it all!
     """
-    pass
+    response = input("Enter a number between " + str(low) + " and " + str(high) + ": ")
+
+    while response.isdigit() == False:
+
+        print("Invalid. Not a number")
+
+        response = input("Invalid. Enter a number between " + str(low) + " and " + str(high) + ": ")
+
+    response = int(response)
+
+    while response <= low or response >= high:
+        
+        response = input("Invalid. Enter a number between " + str(low) + " and " + str(high) + ": ")
+
+        while response.isdigit() == False:
+            
+            print("Invalid. Not a number")
+            
+            response = input("Invalid. Enter a number between " + str(low) + " and " + str(high) + ": ")
+            
+        response = int(response)
+
+    print("Ok")
+    return(response)
 
 
 if __name__ == "__main__":
@@ -84,6 +168,6 @@ if __name__ == "__main__":
     print("\nstubborn_asker")
     stubborn_asker(30, 45)
     print("\nnot_number_rejector")
-    not_number_rejector("Enter a number: ")
+    not_number_rejector("Enter a nunber: ")
     print("\nsuper_asker")
     super_asker(33, 42)
