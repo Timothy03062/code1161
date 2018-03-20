58f50e781b0121f6d4ec0ac967b50f03a6acaeb9
Timothy03062 <36873809+Timothy03062@users.noreply.github.com>
2018-03-20 20:31:44 +1100

    exe3

--- week3/exercise3.py
+++ week3/exercise3.py
@@ -28,6 +28,49 @@ def advancedGuessingGame():
     Remember to think modular. Try to keep your functions small and single
     purpose if you can!
     """
+    def advancedGuessingGame():
+        print("Welcome to the guessing game!")
+    i=input("Set a lower bound: ")
+    while True:
+        try:
+            i= int(i)
+            break
+        except ValueError:
+            i=input('Set a lower bound again: ')
+            continue
+    s=input("Set a higher bound: ")
+    while True:
+        try:
+            s = int(s)
+            while True:
+                if int(s)>int(i):
+                    break
+                else:
+                    s=input("The number should bigger than the low bound, set again: ")
+            break
+        except ValueError:
+            s=input('Set a higher bound again: ')
+            continue
+    print ("Now, guess a number between "+str(i)+" and "+str(s))
+    actualNumber = random.randint(int(i), int(s))
+    guessed = False
+    while not guessed:
+        guessedNumber = input("guess a number: ")
+        while True:
+            try: 
+                guessedNumber=int(guessedNumber)
+                break
+            except ValueError:
+                guessedNumber=input('You should enter a number to guess: ')
+                continue
+        print("you guessed {},".format(guessedNumber),)
+        if int(guessedNumber) == int(actualNumber):
+            print("you got it!! It was {}".format(actualNumber))
+            guessed = True
+        elif int(guessedNumber) < int(actualNumber):
+            print("too small, try again ")
+        else:
+            print("too big, try again  ")  
     return "You got it!"
 
 
